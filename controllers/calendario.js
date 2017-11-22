302a2d2638926fed3ad08fc2eec88a8e3aa95e01
Allan Furlani Nishigouri <anishigouri@oito.srv.br>
2017-11-22 11:28:52 -0200

    CRUD calendario

--- controllers/calendario.js
+++ controllers/calendario.js
@@ -1,15 +1,68 @@
 module.exports = function(app) {
-    app.get('/calendarios', function(req, res) {
-
+    
+        app.get('/calendarios', function(req, res) {
+          var connection = app.persistencia.connectionFactory();
+          var calendarioDAO = new app.persistencia.CalendarioDAO(connection);
+    
+          calendarioDAO.list(function(err, result) {
+              if(err) {
+                  console.log(err);
+                  res.status(500).send(err);
+                  return;
+              }
+    
+              res.send(result);
+          });
+        });
+    
+      app.get('/calendarios/:id', function(req, res) {
+        var id = req.params.id;
         var connection = app.persistencia.connectionFactory();
         var calendarioDAO = new app.persistencia.CalendarioDAO(connection);
-
-        calendarioDAO.lista(function(erro, resultado) {
-            if(erro) {
-                res.status(500).send(erro);
-                return;
-            }
-            res.send(resultado);
+    
+        calendarioDAO.getById(id, function(err, result) {
+          if(err) {
+            res.status(500).send(err);
+            return;
+          }
+          res.send(result);
         });
-    });
-}
+      });
+    
+      app.post('/calendarios', function(req, res) {
+    
+        var connection = app.persistencia.connectionFactory();
+        var calendarioDAO = new app.persistencia.CalendarioDAO(connection);
+        var calendario = req.body['calendario'];
+    
+        calendarioDAO.save(calendario, function(err, result) {
+            if(err) {
+                console.log('Erro ao inserir no banco de dados', err);
+                res.status(500).send(err);
+            } else {
+                console.log('resultado', result);
+                res.status(201).json(result);
+            }
+        });
+
+      });
+    
+      app.put('/calendarios', function(req, res) {
+        
+        var calendario = req.body['calendario'];
+    
+        var connection = app.persistencia.connectionFactory();
+        var calendarioDAO = new app.persistencia.CalendarioDAO(connection);
+    
+        calendarioDAO.update(calendario, function(erro) {
+          if(erro) {
+            res.status(500).send(erro);
+            return;
+          }
+          res.send(calendario);
+        });
+    
+      });
+      
+    }
+      
